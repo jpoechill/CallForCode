9935d03e24670e04eb3f1d4c95ac9e6fd9db0898
Vishal Bakshi <vdbakshi@gmail.com>
2019-05-23 22:07:40 -0700

    update html files

--- WildfireSurvivorManagementSystem/server.js
+++ WildfireSurvivorManagementSystem/server.js
@@ -3,17 +3,18 @@ const dotenv = require("dotenv");
 const app = express();
 const port = 8000;
 const helmet = require("helmet");
-
+  
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
-      defaultSrc: ["'self'"],
-      styleSrc: ["'self'", "maxcdn.bootstrapcdn.com"]
+      defaultSrc: ["'self'", "https://www.gstatic.com"],
+      scriptSrc: ["'self'", "https://www.gstatic.com"],
+      styleSrc: ["'self'"]
     }
   })
 );
 
-app.use(express.static("styles"));
+app.use(express.static(__dirname + "/styles"));
 
 app.route("/").get((req, res) => {
   res.sendFile(__dirname + "/index.html");
